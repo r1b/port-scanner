d44e93a1c6cb66d520467b0fe2812ddd041ae674
r1b <robert.cole.jensen@gmail.com>
2022-06-17 01:35:17 -0400

    feat: store original hostname

--- README.md
+++ README.md
@@ -11,15 +11,11 @@ Ensure that pyenv and pyenv-virtualenv are available on your machine.
 
 ## Features
 
-- [x] Implement connect() scan for cidr + range of ports
+- [x] connect() scan 
 - [x] Distinguish between open / closed / filtered
 - [x] Host discovery with icmplib
-- [ ] Multiple host / network specs and multiple port specs
-  - Overlapping ranges are interesting - nmap by default will scan an address each time it is seen (see --unique)
-  - Not sure how this works for ports
-- [ ] Support rDNS - display original hostname if resolved
-- [ ] Randomize host / port order - still report in requested order
+- [x] Multiple network specs and multiple port specs
+- [x] Display original hostname if resolved
+- [ ] Support rDNS
+- [x] Randomize port order - still report in requested order
 - [x] Parse /etc/services and display corresponding service
-- [ ] Scan delay / max concurrent host connections / other rate limiting
-    - This is the hardest thing to get right
-    - Nmap has an instructive implementation https://github.com/nmap/nmap/blob/df33da47228e3f32f9a332f7db0a0a4f2f14084d/scan_engine.cc#L2779-L2807
--- port_scanner/__init__.py
+++ port_scanner/__init__.py
@@ -1,10 +1,10 @@
 import logging
 import random
 import socket
-from concurrent.futures import as_completed, ThreadPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, as_completed
 from enum import Enum
-from ipaddress import IPv4Address, IPv4Network, IPv6Address, IPv6Network, ip_network
-from typing import Iterable, List
+from ipaddress import IPv4Network, IPv6Network, ip_network
+from typing import Iterable, List, Optional
 
 import typer
 from icmplib import ping
@@ -50,19 +50,34 @@ def parse_ports(port_specs: str) -> Iterable[int]:
     return [i for i in range(1, MAX_PORT + 1) if target_ports[i]]
 
 
-def parse_hostname(hostname) -> IPv4Network | IPv6Network:
+def parse_hostname(hostname: str) -> IPv4Network | IPv6Network:
     addrinfos = socket.getaddrinfo(hostname, None, proto=socket.IPPROTO_TCP)
     # Don't prefer IPv4 or IPv6 - just take the first result
     sockinfo = addrinfos[0][-1]
     address = sockinfo[0]
-    return ip_network(address)
+    return address
 
 
-def parse_network(network) -> Iterable[IPv4Address | IPv6Address]:
-    try:
-        return ip_network(network).hosts()
-    except ValueError:
-        return parse_hostname(network).hosts()
+def parse_targets(network_specs: List[str], port_spec: str):
+    targets = []
+    ports = parse_ports(port_spec)
+
+    for network_spec in network_specs:
+        try:
+            network = ip_network(network_spec)
+            is_hostname = False
+        except ValueError:
+            network = ip_network(parse_hostname(network_spec))
+            is_hostname = True
+
+        hostname = network_spec if is_hostname else None
+
+        targets += [
+            Target(str(host), ports, hostname=hostname)
+            for host in network.hosts()
+        ]
+
+    return targets
 
 
 class Target:
@@ -70,30 +85,44 @@ class Target:
         DOWN = "down"
         UP = "up"
 
-    def __init__(self, host: str, ports: Iterable[int]):
+    def __init__(
+        self, host: str, ports: Iterable[int], hostname: Optional[str] = None
+    ):
         self.host = host
         self.ports = [TargetPort(host, port) for port in ports]
+        self.hostname = hostname
         self.status = None
 
         random.shuffle(self.ports)
 
     def probe(self):
         self.status = self._probe()
-        logger.debug("target probe complete: host=%s status=%s", self.host, self.status)
+        logger.debug(
+            "target probe complete: host=%s status=%s", self.host, self.status
+        )
 
     def _probe(self) -> Status:
         result = ping(self.host, count=1, privileged=False)
         return Target.Status.UP if result.is_alive else Target.Status.DOWN
 
     def report(self) -> str:
-        lines = [f"Host report for {self.host}", f"Host is {self.status.value}"]
+        hostname = f" ({self.hostname})" if self.hostname else ""
+        lines = [
+            f"Host report for {self.host}{hostname}",
+            f"Host is {self.status.value}",
+        ]
 
         # TODO: nmap does something smart where a closed or filtered port
         # is considered notable if its status is a minority
-        notable_ports = [
-            target_port for target_port in self.ports
-            if target_port.status == TargetPort.Status.OPEN
-        ] if self.status == Target.Status.UP else []
+        notable_ports = (
+            [
+                target_port
+                for target_port in self.ports
+                if target_port.status == TargetPort.Status.OPEN
+            ]
+            if self.status == Target.Status.UP
+            else []
+        )
 
         if not notable_ports:
             lines.append("All ports filtered or closed")
@@ -126,13 +155,20 @@ class TargetPort:
 
     def probe(self):
         self.status = self._probe()
-        logger.debug("target port probe complete: host=%s port=%d status=%s", self.host, self.port, self.status)
+        logger.debug(
+            "target port probe complete: host=%s port=%d status=%s",
+            self.host,
+            self.port,
+            self.status,
+        )
 
     def _probe(self) -> Status:
         # Nmap has much more sophisticated logic here
         # Ref: https://github.com/nmap/nmap/blob/df33da47228e3f32f9a332f7db0a0a4f2f14084d/scan_engine_connect.cc#L190-L267
         try:
-            conn = socket.create_connection((self.host, self.port), timeout=CONNECT_TIMEOUT_SECONDS)
+            conn = socket.create_connection(
+                (self.host, self.port), timeout=CONNECT_TIMEOUT_SECONDS
+            )
         except ConnectionRefusedError:
             return TargetPort.Status.CLOSED
         except TimeoutError:
@@ -141,33 +177,32 @@ class TargetPort:
         try:
             conn.close()
         finally:
-            return TargetPort.Status.OPEN
+            pass
+
+        return TargetPort.Status.OPEN
 
 
 @cli.command()
 def main(
     networks: List[str],
-    debug: bool = typer.Option(False, "-d"),
-    ports: str = typer.Option(..., "-p")
+    debug: bool = typer.Option(False, "-d"),  # noqa B008
+    ports: str = typer.Option(..., "-p"),  # noqa B008
 ):
     if debug:
         logger.setLevel(logging.DEBUG)
 
-    hosts = [str(host) for network in networks for host in parse_network(network)]
-    ports = parse_ports(ports)
-
-    targets = {}
-    for host in hosts:
-        targets[host] = Target(host, ports)
+    targets = parse_targets(networks, ports)
 
     # TODO: Restructure the main loop to support rate limiting and other flow control features.
     # Ref: https://github.com/nmap/nmap/blob/df33da47228e3f32f9a332f7db0a0a4f2f14084d/scan_engine.cc#L2779-L2807
     # TODO(perf): Use select / epoll
     with ThreadPoolExecutor(MAX_WORKERS) as executor:
+        # TODO: rDNS
+
         # Host discovery
         host_probe_futures = {}
 
-        for target in targets.values():
+        for target in targets:
             host_probe_futures[executor.submit(target.probe)] = target
 
         for future in as_completed(host_probe_futures):
@@ -178,19 +213,22 @@ def main(
 
         # Port scan
         port_probe_futures = {}
-        for target in targets.values():
+        for target in targets:
             if target.status == Target.Status.DOWN:
                 continue
             for target_port in target.ports:
-                port_probe_futures[executor.submit(target_port.probe)] = target_port
+                port_probe_futures[
+                    executor.submit(target_port.probe)
+                ] = target_port
 
         for future in as_completed(port_probe_futures):
             _ = future.result()
             target_port = port_probe_futures[future]
             if target_port.status == TargetPort.Status.OPEN:
-                typer.echo(f"Discovered open port tcp/{target_port.port} on {target_port.host}")
+                typer.echo(
+                    f"Discovered open port tcp/{target_port.port} on {target_port.host}"
+                )
 
     # Report results in user-specified order
-    for host in hosts:
-        target = targets[host]
+    for target in targets:
         typer.echo(target.report())
